21d42f828c243d64045e3f990efa2657d6af5110
deanspecht <deansp@web.de>
2024-04-22 12:53:49 +0200

    add BuzzCounter

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,32 @@ import {useState} from "react";
 function App() {
     const tipp:string = "Tipp: Baue diese Applikation in React nach. Beachte: Zahlen die durch 3, 5 und 3 und 5 teilbar sind, werden besonders angezeigt."
     const [count, setCount] = useState<number>(0)
+    const [buzzCount, setBuzzCount] = useState<string>("0")
+    const buzzNumbers:string[]=[]
+    for(let i=0;i<=100; i++){
+        if(i%3===0 && i>0){
+            buzzNumbers.push("neue")
+        }else if( i%5===0 && i>0) {
+            buzzNumbers.push("fische")
+        }else{
+            buzzNumbers.push(i.toString())
+        }
+    }
 
     function changeCountPos(){
         setCount(count+1)
+        setBuzzCount(buzzNumbers[count+1])
     }
     function changeCountNeg(){
-        if(count >0)setCount(count-1)
+        if(count >0){
+            setCount(count-1)
+            setBuzzCount(buzzNumbers[count-1])
+        }
     }
 
     return (
       <div className={"container"}>
-          <h1>{count}</h1>
+          <h1>{buzzCount}</h1>
           <h3>{count}</h3>
           <div>
               <button onClick={changeCountNeg} className="buttton">-</button>
