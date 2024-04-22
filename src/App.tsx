ba85c2edc5abb046bd5367ab6464ad53301c317d
deanspecht <deansp@web.de>
2024-04-22 12:06:06 +0200

    add count

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import './App.css'
+import {useState} from "react";
 
 function App() {
-const tipp:string = "Tipp: Baue diese Applikation in React nach. Beachte: Zahlen die durch 3, 5 und 3 und 5 teilbar sind, werden besonders angezeigt."
-  return (
+    const tipp:string = "Tipp: Baue diese Applikation in React nach. Beachte: Zahlen die durch 3, 5 und 3 und 5 teilbar sind, werden besonders angezeigt."
+    const [count, setCount] = useState<number>(0)
+
+    function changeCountPos(){
+        setCount(count+1)
+    }
+    function changeCountNeg(){
+        if(count >0)setCount(count-1)
+    }
+
+    return (
       <div className={"container"}>
-          <h1>0</h1>
-          <h2>0</h2>
-          <div >
-              <button className="buttton">+</button>
-              <button className="buttton">-</button>
+          <h1>{count}</h1>
+          <h3>{count}</h3>
+          <div>
+              <button onClick={changeCountNeg} className="buttton">-</button>
+              <button onClick={changeCountPos} className="buttton">+</button>
           </div>
           {tipp}
       </div>
-  )
+    )
 }
 
 export default App
