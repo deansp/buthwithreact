c00d2777b30044aad6a9e0331467c0dfd4ef0a28
deanspecht <deansp@web.de>
2024-04-22 11:43:08 +0200

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
 
 function App() {
-
+const tipp:string = "Tipp: Baue diese Applikation in React nach. Beachte: Zahlen die durch 3, 5 und 3 und 5 teilbar sind, werden besonders angezeigt."
   return (
-    <>
-
-    </>
+      <div className={"container"}>
+          <h1>0</h1>
+          <h2>0</h2>
+          <div >
+              <button className="buttton">+</button>
+              <button className="buttton">-</button>
+          </div>
+          {tipp}
+      </div>
   )
 }
 
